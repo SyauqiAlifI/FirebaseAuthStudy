a5d961cab93f8e1c374e69dba31e2bf8c2b44324
SyauqiAlifI <vstudio776@gmail.com>
2022-09-19 20:28:20 +0700

    completed step 13 - get user data in splash screen

--- app/src/main/java/com/alif/firebaseauthstudy/MainActivity.kt
+++ app/src/main/java/com/alif/firebaseauthstudy/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.alif.firebaseauthstudy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alif.firebaseauthstudy.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
+
+    private lateinit var fAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initFirebaseAuth()
+        getData()
+
+        binding.btnLogout.setOnClickListener {
+            fAuth.signOut()
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun getData() {
+        val user = fAuth.currentUser
+        if (user != null) {
+            binding.tvEmail.text = user.email
+        }
+    }
+
+    private fun initFirebaseAuth() {
+        fAuth = FirebaseAuth.getInstance()
     }
 }
--- app/src/main/java/com/alif/firebaseauthstudy/SplashActivity2.kt
+++ app/src/main/java/com/alif/firebaseauthstudy/SplashActivity2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,17 @@ class SplashActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_splash2)
 
         Handler().postDelayed({
+            checkAuth()
+        }, 1200)
+    }
+
+    private fun checkAuth() {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
-        }, 1200)
+        }
     }
 }
